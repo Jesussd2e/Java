e2d9d97aa4a386946185b6bd86fddcdc423a6188
jesussd2e <jesusalbeto69@gmail.com>
2024-06-03 14:54:44 -0600

    Practica 3 final...

--- src/Hilos.java
+++ src/Hilos.java
@@ -16,7 +16,6 @@ public class Hilos extends Thread {
                 mainInstance.setDinero(currentDinero - amountToDecrement);
                 long endTime = System.currentTimeMillis();
                 System.out.println(Thread.currentThread().getName() + ": " + mainInstance.getDinero());
-                long duration = endTime - startTime;
                 System.out.println("Inicial: " + startTime + " Final: " + endTime);
             } else {
               System.err.println(Thread.currentThread().getName() + ": No hay suficiente dinero para descontar, Dinero disponible:"+ currentDinero);
