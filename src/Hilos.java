b552e3ee554b68fe973b9614045011e6569b52fb
jesussd2e <jesusalbeto69@gmail.com>
2024-06-03 14:52:56 -0600

    Practica 3 final...

--- src/Hilos.java
+++ src/Hilos.java
@@ -1,16 +1,26 @@
-public class Hilos extends Thread{
-    private int Dinero;
+public class Hilos extends Thread {
+    private final Main mainInstance;
 
-    public int getDinero() {
-        return Dinero;
-    }
-
-    public void setDinero(int dinero) {
-        Dinero = dinero;
+    public Hilos(Main mainInstance) {
+        this.mainInstance = mainInstance;
     }
 
     @Override
-    public void run(){
+    public void run() {
+        synchronized (mainInstance) {
+            long amountToDecrement = 30;
+            long currentDinero = mainInstance.getDinero();
+            long startTime = System.currentTimeMillis();
 
+            if (currentDinero >= amountToDecrement) {
+                mainInstance.setDinero(currentDinero - amountToDecrement);
+                long endTime = System.currentTimeMillis();
+                System.out.println(Thread.currentThread().getName() + ": " + mainInstance.getDinero());
+                long duration = endTime - startTime;
+                System.out.println("Inicial: " + startTime + " Final: " + endTime);
+            } else {
+              System.err.println(Thread.currentThread().getName() + ": No hay suficiente dinero para descontar, Dinero disponible:"+ currentDinero);
+            }
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    long dinero=100;
+    public long getDinero() {
+        return dinero;
+    }
+
+    public void setDinero(long dinero) {
+        this.dinero = dinero;
+    }
     public static void main(String[] args) {
-        long dinero=100;
-        System.out.println(dinero);
+        Main mainInstance = new Main();
+        Hilos hilo1 = new Hilos(mainInstance);
+        Hilos hilo2 = new Hilos(mainInstance);
+        Hilos hilo3 = new Hilos(mainInstance);
+        Hilos hilo4 = new Hilos(mainInstance);
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+        hilo4.start();
     }
 }
